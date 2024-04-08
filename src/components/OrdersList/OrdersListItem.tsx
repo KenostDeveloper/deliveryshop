98f19db33c1906ee9d48a357f05fa3bcfcaae77a
Steiden <baldindaniilza@mail.ru>
2024-04-08 17:07:07 +0500

    upd

--- src/components/OrdersList/OrdersListItem.tsx
+++ src/components/OrdersList/OrdersListItem.tsx
@@ -2,76 +2,47 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import Title from "../UI/Title.components";
 import styles from "./OrdersList.module.scss";
-import { OrderType } from "./types";
 import Link from "next/link";
 
-const OrdersListItem = ({ order }: { order: OrderType }) => {
+const OrdersListItem = ({ order }: { order: any }) => {
     const router = useRouter();
-
-    const dateFormated = new Intl.DateTimeFormat("ru-RU", {
-        day: "numeric",
-        month: "numeric",
-        year: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-    })?.format(new Date(order.date));
 
     return (
         <article
             className={`${styles["orders__item"]}`}
-            onClick={() => router.push(`/orders/${order.id}`)}>
+            onClick={() => router.push(`/orders/${order?.id}`)}>
             <div className={`${styles["orders__info"]}`}>
                 <div className={`${styles["orders__info-block"]}`}>
                     <Link
-                        href={`/orders/${order.id}`}
+                        href={`/orders/${order?.id}`}
                         className={`${styles["orders__info-title"]}`}>
-                        Заказ от {dateFormated}{" "}
-                        <span className={`${styles["orders__info-number"]}`}>#{order.id}</span>
+                        Заказ от {order?.date}{" "}
+                        <span className={`${styles["orders__info-number"]}`}>#{order?.id}</span>
                     </Link>
-                    <p className={`${styles["orders__info-title"]}`}>
-                        {order.products.reduce((acc, item) => (acc += item.price), 0)}₽
-                    </p>
+                    <p className={`${styles["orders__info-title"]}`}>{order?.cost}₽</p>
                 </div>
                 <div className={`${styles["orders__info-block"]}`}>
-                    <p className={`${styles["orders__info-text"]}`}>{order.deliveryMethod}</p>
-                    <p className={`${styles["orders__info-text"]}`}>{order.status}</p>
+                    <p className={`${styles["orders__info-text"]}`}>{order?.deliveryMethod}</p>
+                    <p className={`${styles["orders__info-text"]}`}>{order?.status}</p>
                 </div>
             </div>
             <div className={`${styles["orders__products"]}`}>
-                {order.products.length > 5
-                    ? order.products.map((product, index) => {
-                          if (index >= 5)
-                              return (
-                                  <article className={`${styles["orders__more"]}`}>
-                                      +{order.products.length - 5}
-                                  </article>
-                              );
-                          return (
-                              <article className={`${styles["orders__product"]}`} key={product.id}>
-                                  <img
-                                      src={product.image}
-                                      alt="product image"
-                                      className={`${styles["orders__product-img"]}`}
-                                  />
-                              </article>
-                          );
-                      })
-                    : order.products.map((product, index) => {
-                          return (
-                              <article className={`${styles["orders__product"]}`} key={product.id}>
-                                  <img
-                                      src={product.image}
-                                      alt="product image"
-                                      className={`${styles["orders__product-img"]}`}
-                                  />
-                                  <p className={`${styles["orders__product-text"]}`}>
-                                      {product.name}
-                                  </p>
-                              </article>
-                          );
-                      })}
+                {order?.products?.map((product: any, index: number) => {
+                    if (index >= 5) return null;
+                    return (
+                        <article className={`${styles["orders__product"]}`} key={product?.id}>
+                            <img
+                                src={`/products/${product?.product?.image}`}
+                                alt="product image"
+                                className={`${styles["orders__product-img"]}`}
+                            />
+                        </article>
+                    );
+                })} 
+                <article className={`${styles["orders__more"]}`}>
+                    +{order?.products?.length - 5}
+                </article>
             </div>
         </article>
     );
